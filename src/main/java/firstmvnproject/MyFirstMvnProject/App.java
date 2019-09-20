f029fd0a9f6fd6fdc2d203b4d16f1e8f6b279a77
Amit <amitns88@gmail.com>
2019-09-20 21:04:30 +0530

    updated for test

--- src/main/java/firstmvnproject/MyFirstMvnProject/App.java
+++ src/main/java/firstmvnproject/MyFirstMvnProject/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World New!" );
+        System.out.println( "Hello World New with test!" );
     }
     
     public String getMessage() {
